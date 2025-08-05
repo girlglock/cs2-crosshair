c924f8c45bbc7f6aaeb02c2ab4e3249e40b5877e
deabb <grcjn.b.rcki@gmail.com>
2025-08-05 23:41:23 +0200

    qol: add clickable usernames that lead to steam or leetify profiles

--- services/templateService.js
+++ services/templateService.js
@@ -69,18 +69,34 @@ function generateHTML(crosshairData, settings, imageUrl, errorType = '', origina
     const [r, g, b] = settings ? getColorFromSettings(settings) : [0, 255, 0];
     const themeColor = `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
 
-    const title = crosshairData.crosshairCode === originalInput 
-        ? 'cs2-crosshair-viewer' 
+    const title = crosshairData.crosshairCode === originalInput
+        ? 'cs2-crosshair-viewer'
         : `${crosshairData.nickname}'s crosshair`;
 
-    const playerInfo = crosshairData.nickname && crosshairData.cs2Hours 
+    const playerEmbedInfo = crosshairData.nickname && crosshairData.cs2Hours
         ? `${crosshairData.nickname} • ${crosshairData.cs2Hours} \n`
         : crosshairData.nickname + "\n" || '';
+
+    const isLeetify = !crosshairData.steamId64;
+    const profileUrl = isLeetify
+        ? `https://leetify.com/@${crosshairData.nickname}`
+        : `https://steamcommunity.com/profiles/${crosshairData.steamId64}`;
+
+    const iconUrl = isLeetify
+        ? 'https://leetify.com/assets/images/favicon.svg'
+        : 'https://steamcommunity.com/favicon.ico';
+
+    const playerInfo = crosshairData.nickname
+        ? `<a href="${profileUrl}" style="text-decoration: none; display: inline-flex; align-items: center;">
+        <img src="${iconUrl}" style="width: 14px; height: 14px; margin-right: 6px;">
+        ${crosshairData.nickname}${crosshairData.cs2Hours ? ` • ${crosshairData.cs2Hours}` : ''}</a>`
+        : '';
 
     const templateName = isBot ? 'discord-embed' : 'crosshair-preview';
 
     return renderTemplate(templateName, {
         title,
+        playerEmbedInfo,
         playerInfo,
         codeValue: isBot && crosshairData.crosshairCode === "CSGO-PfaBQ-tbEjf-aeQtY-fF6hh-kESyL" ? 'search and view cs2 player crosshairs' : crosshairData.crosshairCode,
         originalInput,
